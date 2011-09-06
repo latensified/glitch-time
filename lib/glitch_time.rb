e3605bfd7e21ccb66ac56146a9e74c6d931f3189
Simon Peck <speck@quantcast.com>
2011-09-06 01:35:39 -0700

    Added funtionality for pretty printing dates and times. My results still do not agree with the official Glitch world clock.

--- lib/glitch_time.rb
+++ lib/glitch_time.rb
@@ -1,14 +1,45 @@
+#
+# Converts real time to Glitch Time
+#
+# There are 308 days in a glitch year.
+# There are 4435200 real seconds in a game year.
+# There are 14400 real seconds in a game day.
+# There are 600 real seconds in a game hour.
+# There are 10 real seconds in a game minute.
+#
+# The Glitch epoch began at 1238562000 Unix time.
+
 class GlitchTime
   GAME_EPOCH = 1238562000
+  DAYS_IN_YEAR = 308
+  HOURS_IN_HALF_DAY = 12
   SECONDS_IN_GAME_YEAR = 4435200
   SECONDS_IN_GAME_DAY = 14400
   SECONDS_IN_GAME_HOUR = 600
   SECONDS_IN_GAME_MINUTE = 10
+  MONTHS = {'Primuary' => 29,
+            'Spork' => 3,
+            'Bruise' => 53,
+            'Candy' => 17,
+            'Fever' => 73,
+            'Junuary' => 19,
+            'Septa' => 13,
+            'Remember' => 37,
+            'Doom' => 5,
+            'Widdershins' => 47,
+            'Eleventy' => 11,
+            'Recurse' => -1}
+
+  DAYS = ['Hairday', 'Moonday', 'Twoday', 'Weddingday', 'Theday', 'Fryday', 'Standay', 'Fabday']
 
   attr_reader :real_now
 
   def initialize
     @real_now = Time.now.to_i
+    @year_offset = (year * SECONDS_IN_GAME_YEAR)
+    @day_of_year_offset = (day_of_year * SECONDS_IN_GAME_DAY)
+    @hour_offset = (hour * SECONDS_IN_GAME_HOUR)
+    @minute_offset = (minute * SECONDS_IN_GAME_MINUTE)
   end
 
   def now
@@ -20,21 +51,89 @@ class GlitchTime
   end
 
   def day_of_year
-    sec = now - (year * SECONDS_IN_GAME_YEAR)
-    (sec / SECONDS_IN_GAME_DAY).floor
+    offset = now - @year_offset
+    (offset / SECONDS_IN_GAME_DAY).floor
   end
 
   def hour
-    sec = now - (year * SECONDS_IN_GAME_YEAR) - (day_of_year * SECONDS_IN_GAME_DAY)
-    (sec / SECONDS_IN_GAME_HOUR).floor
+    offset = now - @year_offset - @day_of_year_offset
+    (offset / SECONDS_IN_GAME_HOUR).floor
+  end
+
+  def standard_hour
+    current_hour = hour
+    current_hour > HOURS_IN_HALF_DAY ? current_hour -= HOURS_IN_HALF_DAY : current_hour
+  end
+
+  def meridian_indicator
+    current_hour = hour
+    current_hour < HOURS_IN_HALF_DAY ? 'am' : 'pm'
   end
 
   def minute
-    sec = now - (year * SECONDS_IN_GAME_YEAR) - (day_of_year * SECONDS_IN_GAME_DAY) - (hour * SECONDS_IN_GAME_HOUR)
-    (sec / SECONDS_IN_GAME_MINUTE).floor
+    offset = now - @year_offset - @day_of_year_offset - @hour_offset
+    (offset / SECONDS_IN_GAME_MINUTE).floor
+  end
+
+  def pretty_minute
+    "%02d" % minute
   end
 
   def second
-    now - (year * SECONDS_IN_GAME_YEAR) - (day_of_year * SECONDS_IN_GAME_DAY) - (hour * SECONDS_IN_GAME_HOUR) - (minute * SECONDS_IN_GAME_MINUTE)
+    now - @year_offset - @day_of_year_offset - @hour_offset - @minute_offset
+  end
+
+  def days_since_epoch
+    day_of_year + (DAYS_IN_YEAR * year)
+  end
+
+  def day_of_week
+    days_since_epoch.modulo 8;
+  end
+
+  def day_of_month
+    day_to_month_day(day_of_year)[1]
+  end
+
+  def month_of_year
+    day_to_month_day(day_of_year)[0]
+  end
+
+  def day_to_month_day incremental_day
+    current_day = 0
+    m = 0
+
+    MONTHS.each do |month|
+      current_day += month[1]
+      if (current_day > incremental_day)
+        m += 1
+        d = incremental_day + 1 - (current_day - month[1])
+        return [m, d]
+      end
+    end
+    array(0, 0)
+  end
+
+  def day_name
+    DAYS[day_of_week]
+  end
+
+  def month_name
+    MONTHS.to_a[day_to_month_day(day_of_year)[0]][0]
+  end
+
+  def suffix value = day_of_month
+    case value
+      when 0
+        'th'
+      when 1
+        'st'
+      when 2
+        'nd'
+      when 3
+        'rd'
+      else
+        'th'
+    end
   end
 end
--- test/glitch_time_test.rb
+++ test/glitch_time_test.rb
@@ -26,7 +26,7 @@ class GlitchTimeTest < Test::Unit::TestCase
   def test_glitch_time
     glitch_time = GlitchTime.new
 
-    sec = glitch_time.real_now - 1238562000;
+    sec = glitch_time.real_now - 1238562000
 
     # there are 4435200 real seconds in a game year
     # there are 14400 real seconds in a game day
@@ -46,7 +46,7 @@ class GlitchTimeTest < Test::Unit::TestCase
     hour = (sec / 600).floor
 
     assert_equal(hour, glitch_time.hour, "Hour is incorrect.")
-    
+
     sec -= hour * 600
     minute = (sec / 10).floor
 
@@ -55,6 +55,85 @@ class GlitchTimeTest < Test::Unit::TestCase
     sec -= minute * 10
 
     assert_equal(sec, glitch_time.second, "Seconds are incorrect.")
+
+  end
+
+  def test_days_since_epoch
+    glitch_time = GlitchTime.new
+    expected = glitch_time.now / 14400
+
+    assert_equal(expected, glitch_time.days_since_epoch, "Days since epoch is incorrect.")
+  end
+
+  def test_day_to_month_day
+    glitch_time = GlitchTime.new
+
+
+    puts "Today is #{glitch_time.month_of_year}/#{glitch_time.day_of_month}!"
+
+    third_of_primuary = [1, 3]
+    actual = glitch_time.day_to_month_day(2)
+
+    assert_equal(third_of_primuary, actual)
+
+    first_of_spork = [2, 1]
+    actual = glitch_time.day_to_month_day(29)
+
+    fifty_third_of_bruise = glitch_time.day_to_month_day(84)
+  end
+
+  def test_suffix
+    glitch_time = GlitchTime.new
+
+    expected = 'th'
+    actual = glitch_time.suffix 0
+    assert_equal(expected, actual, "0 failed suffix test")
+
+    expected = 'st'
+    actual = glitch_time.suffix 1
+    assert_equal(expected, actual, "1 failed suffix test")
+
+    expected = 'nd'
+    actual = glitch_time.suffix 2
+    assert_equal(expected, actual, "2 failed suffix test")
+
+    expected = 'rd'
+    actual = glitch_time.suffix 3
+    assert_equal(expected, actual, "3 failed suffix test")
+
+    expected = 'th'
+    actual = glitch_time.suffix 4
+    assert_equal(expected, actual, "4 failed suffix test")
+
+    expected = 'th'
+    actual = glitch_time.suffix 5
+    assert_equal(expected, actual, "5 failed suffix test")
+
+    expected = 'th'
+    actual = glitch_time.suffix 6
+    assert_equal(expected, actual, "6 failed suffix test")
+
+    expected = 'th'
+    actual = glitch_time.suffix 7
+    assert_equal(expected, actual, "7 failed suffix test")
+
+    expected = 'th'
+    actual = glitch_time.suffix 8
+    assert_equal(expected, actual, "8 failed suffix test")
+
+    expected = 'th'
+    actual = glitch_time.suffix 9
+    assert_equal(expected, actual, "9 failed suffix test")
+
+    expected = 'th'
+    actual = glitch_time.suffix 10
+    assert_equal(expected, actual, "10 failed suffix test")
+  end
+
+  def test_pretty_printing
+    glitch_time = GlitchTime.new
+
+    puts "Right at this very moment, it's #{glitch_time.standard_hour}:#{glitch_time.pretty_minute} #{glitch_time.meridian_indicator}, #{glitch_time.day_name} #{glitch_time.day_of_month}#{glitch_time.suffix} of #{glitch_time.month_name}, year #{glitch_time.year}"
 
   end
 end
